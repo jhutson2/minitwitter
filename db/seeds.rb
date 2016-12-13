f27bff0c9fcec4b24187b005bcd6258ff3c79621
Jonthan Hutson <jhut110766@gmail.com>
2016-12-13 10:32:14 -0500

    minitwitter

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tweets = Tweet.create(
+[
+  { text:'my president was black 0.o', username: '@jeezy103' }
+  { text:'Who likes orange soda? Kell likes orange soda!', username: '@Kell36' }
+]
+)
